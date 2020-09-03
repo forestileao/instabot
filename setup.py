cf128f0cd0e243b377e2579ced7a79f4057cadf9
Pedro Leao <pedroforestileao@gmail.com>
2020-09-03 14:00:32 -0300

    first commit: package structure added

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup
 setup(
     name='insta-bot',
     version='0.0.0',
-    description='An Instagram Bot that follows users that could
-    follow your account and unfollows who doesn\'t follows you.',
+    description='An Instagram Bot that follows users that could follow your account and unfollows who doesn\'t follows you.',
     packages=[
         'insta_bot',
     ],
