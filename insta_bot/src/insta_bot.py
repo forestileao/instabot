032c5aecdbbdc13fe01782b7acf22ec88bd0f532
Pedro Leao <pedroforestileao@gmail.com>
2020-09-09 12:21:31 -0300

    main: main execution file included insta_bot: authentication feature running

--- insta_bot/src/insta_bot.py
+++ insta_bot/src/insta_bot.py
@@ -1,45 +1,52 @@
-from time import time as tm
+from time import time
 import requests
+import base64
+import json
+
+
 
 class InstaBot:
-    def __init__(self, username='mff.tester', password='MhG6reYKyK4C2PC'):
+    def __init__(self, username, password):
         self.username = username
         self.password = password
         self.base_url = 'https://www.instagram.com'
-    
+   
+
+    def generate_encrypted_password(self):
+        str_time = str(int(time()))
+        return '#PWD_INSTAGRAM_BROWSER:0:' + str_time + ':' + self.password
+
 
     def login(self):
         login_url = self.base_url + '/accounts/login/ajax/'
         user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)\
             Chrome/59.0.3071.115 Safari/537.36'
 
-        #Setting some headers and refers
-        session = requests.Session()
-        session.headers = {'user-agent': user_agent}
-        session.headers.update({'Referer': self.base_url})
-
+        #Setting some headers
+        self.session = requests.Session()
+        self.session.headers = {'user-agent': user_agent}
+        self.session.headers.update({'Referer': self.base_url})
+        enc_password = self.generate_encrypted_password()
 
         try:
             #Requesting the base url. Grabbing and inserting the csrftoken
-
-            req = session.get(self.base_url)
-            session.headers.update({'x-csrftoken': req.cookies['csrftoken']})
-            login_data = {'username': self.username, 'encpassword':'#PWD_INSTAGRAM_BROWSER:10:' + str(int(tm())) + ':' + self.password}
+            req = self.session.get(self.base_url)
+            self.session.headers.update({'x-csrftoken': req.cookies['csrftoken']})
+            login_data = {'username': self.username, 'enc_password': enc_password}
 
             #Finally login in
-            login = session.post(login_url, data=login_data, allow_redirects=True)
-            session.headers.update({'x-csrftoken': login.cookies['csrftoken']})
+            login = self.session.post(login_url, data=login_data, allow_redirects=True)
+            self.session.headers.update({'x-csrftoken': login.cookies['csrftoken']})
+            login = json.loads(login.content.decode('utf-8'))
 
-            cookies = login.cookies
-
-            #Print the html results after I've logged in
-            print(login.text)
+            if login['authenticated']:
+                self.user_id = login['userId']
+                print(f'> Logged in (user:{self.username})')
+            else:
+                print('> Erron on login authentication')
+            return login
 
         #In case of refused connection
         except requests.exceptions.ConnectionError:
-            print("Connection refused")
+            print("> Connection refused")
 
-
-if __name__ == "__main__":
-    bot = InstaBot()
-    bot.login()
---
+++ insta_bot/src/main.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__":
+    bot = InstaBot(username='cbbottest', password='thisiscrazy')
+    bot.login()
+
