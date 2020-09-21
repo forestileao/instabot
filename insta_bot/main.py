399ec2315d2b0bf584f72aafd4ce2ba3c7d4d04e
Pedro LeÃao <pedroforestileao@gmail.com>
2020-09-21 01:08:42 -0300

    Added verbose mode

--- insta_bot/main.py
+++ insta_bot/main.py
@@ -5,5 +5,5 @@ from src.insta_bot import InstaBot
 if __name__ == "__main__":
     with open('./credentials.json') as json_file:
         cred = json.load(json_file)
-        bot = InstaBot(username=cred['username'], password=cred['password'], target_username=cred['user_target'])
-    bot.start()
+        bot = InstaBot(username=cred['username'], password=cred['password'])
+    bot.start(target_username=cred['target_username'], verbose=False)
