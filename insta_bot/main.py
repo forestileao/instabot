7d3f3a3a946515b5af85efeb24dcab32efc5b92d
Pedro LeÃao <pedroforestileao@gmail.com>
2020-09-20 23:39:36 -0300

    set user credentials by credentials.json

--- insta_bot/main.py
+++ insta_bot/main.py
@@ -1,9 +1,9 @@
+import json
 from src.insta_bot import InstaBot
 
 
 if __name__ == "__main__":
-    bot = InstaBot(username='cbbottest', password='thisiscrazy')
-    bot.login()
-    bot.map_followers()
+    with open('./credentials.json') as json_file:
+        cred = json.load(json_file)
+        bot = InstaBot(username=cred['username'], password=cred['password'], target_username=cred['user_target'])
     bot.start()
-
